8539b250b40e100b5c72f67ea805e591ed8952dc
Ashenafi <asheaddis@yahoo.com>
2019-09-05 17:00:38 +0200

    Updates

---
+++ precache-manifest.561a25aa6b5c056d4b2d2809abc186ae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1e9ed23260ce757e05b0feb6f91637d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0d5fccba7e8659e0e424",
+    "url": "./static/css/main.9b6bc47a.chunk.css"
+  },
+  {
+    "revision": "fffb722b806d1930ad11",
+    "url": "./static/js/2.41cb42ea.chunk.js"
+  },
+  {
+    "revision": "0d5fccba7e8659e0e424",
+    "url": "./static/js/main.e51c7551.chunk.js"
+  },
+  {
+    "revision": "297ec560c542a35457c3",
+    "url": "./static/js/runtime~main.b206fb79.js"
+  },
+  {
+    "revision": "04be8ab013e5fdf4c6d87ce35966bf95",
+    "url": "./static/media/countryside.04be8ab0.jpg"
+  }
+]);
